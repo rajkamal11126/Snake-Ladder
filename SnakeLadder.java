be6ded4af0634f43d1c9f4bb4cb4f86af4880f06
Raj <rajkamal11126@gmail.com>
2020-10-16 18:17:49 +0530

    check win position

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -8,10 +8,12 @@ public class SnakeLadder
 	final static int NO_PLAY=0;
 	final static int LADDER=1;
 	final static int SNAKE=2;
-	static int playerPosition=INITIAL_POSITION;
+	static int playerOnePosition=INITIAL_POSITION;
 	static int numOfPlayer=1;
 	static int counter=0;
 	static double diceNumber=Math.floor(Math.random()*10)%6+1;
+
+	//check random
 	public static void playGame()
 	{
 		counter=counter+1;
@@ -19,18 +21,35 @@ public class SnakeLadder
 		switch((int)option)
 		{
 			case NO_PLAY :
-				playerPosition=playerPosition;
+				playerOnePosition=playerPosition;
 				break;
 			case LADDER :
-				playerPosition=playerPosition + (int)diceNumber;
+				playerOnePosition=playerPosition + (int)diceNumber;
 				break;
 			case SNAKE :
-				playerPosition=playerPosition - (int)diceNumber;
+				playerOnePosition=playerPosition - (int)diceNumber;
+		}
+		checkReachedWin();
+	}
+	//check Win Position
+	public static void checkReachdWin()
+	{
+		if(playerOnePosition <= INITIAL_POSITION)
+		{
+			playerOnePosition=INITIAL_POSITION;
+		}
+		else if(playerOnePosition >= FINAL_POSITION)
+		{
+			playerOnePosition=FINAL_POSITION;
 		}
 	}
 	//main
 	public static void main(String[] args)
 	{
-		playGame();
+		while(playerOnePosition < FINAL_POSITION)
+		{
+			playGame();
+		}
 	}
 }
+
