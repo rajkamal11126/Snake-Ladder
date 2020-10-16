1a4cdea8dc6842444b481b14f774ef0157bce4d8
Raj <rajkamal11126@gmail.com>
2020-10-16 17:25:19 +0530

    added roll die

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -4,4 +4,13 @@ public class SnakeLadder
 {
 	final static int INITIAL_POSITION=0;
 	final static int numOfPlayer=1;
+	static double rollDie=Math.floor(Math.random()*10)%6+1;
+	public static void rollDie()
+	{
+		System.out.println(rollDie);
+	}
+	public static void main(String[] args)
+	{
+		rollDie();
+	}
 }
